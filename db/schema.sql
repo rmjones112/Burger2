2df3c99e83d73216ac226c2633414350e1a74b3d
rmjones112 <rmjones112gmail.com>
2019-10-31 13:40:22 -0700

    changed feat

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+  create table burgers_tbl(
+    id int primary key auto_increment,
+    burger_name varchar(30) not null,
+    devoured boolean,
+    burger_time timestamp
+  );
